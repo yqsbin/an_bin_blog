4df8534f9627fa4d7f1f74d5144d96b5c8bce530
yqsbin <spring_1024@outlook>
2022-06-15 22:45:08 +0800

    init code

---
+++ yqs-gateway/src/main/java/yqs/blog/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package yqs.blog.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 网关服务
+ * @author yuanbin
+ * @since 2022/06/15
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
